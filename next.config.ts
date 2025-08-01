1228ac2c35a2155459685667b67ee515a9f2773a
The-morning-star23 <kumarshubh263@gmail.com>
2025-08-01 23:48:26 +0530

    updated next.cnfig.ts and installed worker-loader package

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.worker\.ts$/,
+        loader: "worker-loader",
+        options: {
+          filename: "static/chunks/[name].[contenthash].js",
+          publicPath: "/_next/",
+        },
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
