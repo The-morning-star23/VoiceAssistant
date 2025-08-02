83901b5660afe532185ca20cef08f3cf0e535589
The-morning-star23 <kumarshubh263@gmail.com>
2025-08-02 15:02:12 +0530

    updated next.config.ts, simplify worker config

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,10 @@
+// next.config.ts (FINAL, SIMPLIFIED VERSION)
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.module.rules.push({
-        test: /\.worker\.ts$/,
-        loader: "worker-loader",
-        options: {
-          filename: "static/chunks/[name].[contenthash].js",
-          publicPath: "/_next/",
-        },
-      });
-    }
-    return config;
-  },
+  // No special webpack configuration is needed for modern Next.js workers.
+  // We are removing the custom 'worker-loader' rule.
 };
 
 export default nextConfig;
